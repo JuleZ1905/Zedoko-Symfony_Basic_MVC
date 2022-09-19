82b0638fd20788a872d3f7b9c0129ac510c83649
Nekonyx13 <corvin.prath@gmail.com>
2022-09-19 10:22:11 +0200

    for julian

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class DemoController extends AbstractController
 {
-    #[Route('/lucky/number')]
+    #[Route('/demo')]
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
+        return $this->render('demo.html.twig', [
             'number' => $number,
         ]);
     }
