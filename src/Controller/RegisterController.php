6c52fb22f1523b46c87bf23d35b078bd829b595e
Nekonyx13 <corvin.prath@gmail.com>
2022-10-09 17:08:12 +0200

    register form finished

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    #[Route('/register', name: 'register')]
     public function new(Request $request): Response
     {
         $user = new User();
-        $form = $this->createFormBuilder($user)
-            ->add('username')
-            ->add('password')
-            ->getForm();
+        $form = $this->createForm(UserType::class, $user);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            var_dump($user);
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
